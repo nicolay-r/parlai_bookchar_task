4d1c3d2afd3c512f21f29f72a4c4e22dde805301
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-03 20:12:10 +0300

    setup for desktop

--- setup_desktop.sh
+++ setup_desktop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-parlai_dir="/home/nicolay/ParlAI/"
-parlai_project=$parlai_dir"parlai/"
+parlai_project="/media/nicolay/96ed6537-b931-4f7e-8ac4-8407527ddbf9/proj/chatbot-experiments/venv/lib/python3.8/site-packages/parlai"
 task_name="gutenbergbookchars"
 task_dirname="GutenbertBookChars"
 
@@ -9,5 +8,3 @@ target=$parlai_project"/tasks/"$task_name"/"
 
 mkdir -p $target
 cp -r ./* $target
-
-cd $parlai_project"/scripts/" && python display_data.py --task $task_name
