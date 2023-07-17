3e5ef81e1a3114a564b4f1b8e478582eb77b043c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-17 13:56:48 +0100

    Added datasets with spectrums

--- build.py
+++ build.py
@@ -14,7 +14,17 @@ RESOURCES = [
         "https://www.dropbox.com/scl/fi/gloua0tlfmsfvxy6m24cf/dataset_parlai_valid_original.txt.zip?rlkey=wopd1fcpbwz6uxsedydg87s6i&dl=1",
         'dataset_parlai_valid_original.txt.zip',
         'f8dccade663139857cc2db847b8c6ec51fba493a7c7f080efad02e29aed8a459',
-    )
+    ),
+    DownloadableFile(
+        "https://www.dropbox.com/scl/fi/o80budh6xxqwgizie7cef/dataset_parlai_valid_spectrums.txt.zip?rlkey=si4ddvw5be2cfboplt3krdda7&dl=1",
+        'dataset_parlai_valid_spectrums.txt.zip',
+        'f8561b2a80dec4ee65e41d35bf30a18266bc624971bf80aa6176229f13350f08',
+    ),
+    DownloadableFile(
+        "https://www.dropbox.com/scl/fi/8tcjc49dsr4ditsc20o6i/dataset_parlai_train_spectrums.txt.zip?rlkey=ws0f7gparmujwcldquqalm28n&dl=1",
+        'dataset_parlai_train_spectrums.txt.zip',
+        '73b4f01c5061fff2c4fa2af89c435f49f5338d776a8ce5eec7656bbb435e78b1',
+    ),
 ]
 
 TASK_NAME = 'GutenbertBookChars'
