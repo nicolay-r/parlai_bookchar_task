f359b2be61929c525617756ce0cc80014892f995
Subin Jung <82886152+SubinJung-CS@users.noreply.github.com>
2023-07-21 11:12:53 +0100

    Update build.py

--- build.py
+++ build.py
@@ -6,14 +6,14 @@ import parlai.core.build_data as build_data
 
 RESOURCES = [
     DownloadableFile(
-        "https://www.dropbox.com/scl/fi/v43ee83iup1usdhw6zhxa/utterance_speaker_train.txt.zip?rlkey=43mb18p84l3qyz5cugueisbmo&dl=1",
+        "https://www.dropbox.com/scl/fi/nff5gn7r300pp25wwlsqx/utterance_speaker_train.txt.zip?rlkey=1gu8sk5lo1k7yz500wb5dhzik&dl=1",
         'utterance_speaker_train_original.txt.zip',
-        'e0acb6d8638a370460493297bc58b9b364adc691d77a7a599124c6e8529daa71'
+        '505a1f0984e6fc71ee2614baa03980696ec07d2e70942e86aedd2d5b5d26e68e'
     ),
     DownloadableFile(
-        "https://www.dropbox.com/scl/fi/urjt8umkqh6r1hemgnr32/utterance_speaker_test.txt.zip?rlkey=159i9bi89wsmhao5dok7p7msk&dl=1",
+        "https://www.dropbox.com/scl/fi/m8ni8k117tf471g13nu84/utterance_speaker_test.txt.zip?rlkey=3p4ot0dfu3tvxmrihim38993y&dl=1",
         'utterance_speaker_valid_original.txt.zip',
-        'a891a9d5ac3eb92a3e9ee77dac0d0a3fbda774317c975eed3f38fd0b27a8cd2d'
+        'bed64b031752248080aa7443e460c82a03ed637091ce99b065432db3b74925aa'
     ),
 ]
 
