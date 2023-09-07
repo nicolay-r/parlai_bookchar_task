530341c880f40795200cd63e422ddddb661fcddd
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-09-07 16:48:47 +0100

    Update build.py

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import parlai.core.build_data as build_data
 
 
 RESOURCES = [
+    # Data for pre-training in dialogue format without spectrums (original), and with HLA (spectrum)
     DownloadableFile(
         "https://www.dropbox.com/scl/fi/b1ukv0orjgmdg6slcltmj/dataset_parlai_train_dialog_original.txt.zip?rlkey=g81ontblxg8ze8wa195pu6ntw&dl=1",
         'dataset_parlai_train_dialog_original.txt.zip',
@@ -14,7 +15,18 @@ RESOURCES = [
         "https://www.dropbox.com/scl/fi/jnpj8acsjhoq9cgj4x0zj/dataset_parlai_train_dialog_spectrum.txt.zip?rlkey=jvjly9szxbn24lh1dy0c0ksmd&dl=1",
         'dataset_parlai_train_dialog_spectrum.txt.zip',
         '3732c3f5ea35de96e5950f983fc0f569be67337d63865dc08efe48a03acc75df'
-    )
+    ),
+    # Data for Speaker-Classification/Extraction problem
+    DownloadableFile(
+        "https://www.dropbox.com/scl/fi/r241a1ma2douus965h7lf/dataset_parlai_train_hla.txt.zip?rlkey=dwcnm0yxn2boujomd53nx0595&dl=1",
+        'dataset_parlai_train_hla.txt.zip',
+        '36a6d26196b748430ed2e166e5dde90b521f5272cc490ca7541671df62552c67'
+    ),
+     DownloadableFile(
+        "https://www.dropbox.com/scl/fi/arzub1tmegklkf93dthpr/dataset_parlai_valid_hla.txt.zip?rlkey=lpa8vcs48f3bxegk3gtw22h2i&dl=1",
+        'dataset_parlai_valid_hla.txt.zip',
+        '92250678095d568b13a08b68c584c1d32058bbabb0bdf82940bfbfbe3013ac54'
+    ),
 ]
 
 TASK_NAME = 'GutenbergSR'
