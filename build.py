05d1086406d6fdd17735a72849785a172071db00
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-09-20 20:06:26 +0100

    Added test speakers

--- build.py
+++ build.py
@@ -32,10 +32,36 @@ RESOURCES = [
         'dataset_parlai_train_hla.txt.zip',
         '36a6d26196b748430ed2e166e5dde90b521f5272cc490ca7541671df62552c67'
     ),
-     DownloadableFile(
+    DownloadableFile(
         "https://www.dropbox.com/scl/fi/arzub1tmegklkf93dthpr/dataset_parlai_valid_hla.txt.zip?rlkey=lpa8vcs48f3bxegk3gtw22h2i&dl=1",
         'dataset_parlai_valid_hla.txt.zip',
         '92250678095d568b13a08b68c584c1d32058bbabb0bdf82940bfbfbe3013ac54'
+    ),
+    # Test speakers.
+    DownloadableFile(
+        "https://www.dropbox.com/scl/fi/5gqbauw3bp3mnvkfkvh21/153_2.parlai_dataset.txt.zip?rlkey=a0rzgilfdq4vao7oy4cibt2ew&dl=1",
+        '153_2.parlai_dataset.txt.zip',
+        '6753d7674c24bfd6343af5b05cae1446cee7b078c912feb60a8d5819ab38f3ce'
+    ),
+    DownloadableFile(
+        "https://www.dropbox.com/scl/fi/0z1fwabtqv2fjsxdhjel1/403_3.parlai_dataset.txt.zip?rlkey=jsnymkkxs10b2j8r7ewfxpw6k&dl=1",
+        '403_3.parlai_dataset.txt.zip',
+        '0224d828cd896e5b055d5ed8c4bc993a93a32839cc84f60f2abd07c66338d148'
+    ),
+    DownloadableFile(
+        "https://www.dropbox.com/scl/fi/w2y28hhpkral36uk5rby1/1257_7.parlai_dataset.txt.zip?rlkey=wm7mue848kzr4yubd7m9idpew&dl=1",
+        '1257_7.parlai_dataset.txt.zip',
+        '94663677cfae393066f9759ce7935f025b1e8080c12a5f50bfc24b28c598828c'
+    ),
+    DownloadableFile(
+        "https://www.dropbox.com/scl/fi/8a56x1oviz15w4ppi1v9q/1257_9.parlai_dataset.txt.zip?rlkey=ysnibgk3j68nedtlp3g6oc2ew&dl=1",
+        '1257_9.parlai_dataset.txt.zip',
+        '9e07c5a616d635dcdf1aa16cc8371e2581556f901ff0b41f2bd2059f923e3483'
+    ),
+    DownloadableFile(
+        "https://www.dropbox.com/scl/fi/vpeq6r29zoja352eabe99/1258_8.parlai_dataset.txt.zip?rlkey=f8y9261af2bu1pameffw14bzc&dl=1",
+        '1258_8.parlai_dataset.txt.zip',
+        '740aaf2a1f5f01a00241bd62a401f54f673787af8498a6ee37c0403daf2c8c4f'
     ),
 ]
 
